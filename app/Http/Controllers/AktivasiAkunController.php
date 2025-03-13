5b8fa5b7b7b676705ea12d5a705e8d8d618fc881
Rio Pranata Saputra <riopranata.saputra.rps@gmail.com>
2025-03-13 21:20:55 +0700

    UUU

--- app/Http/Controllers/AktivasiAkunController.php
+++ app/Http/Controllers/AktivasiAkunController.php
@@ -13,7 +13,7 @@ class AktivasiAkunController extends Controller
     public function index()
     {
         $data = [
-            'title' => 'AKTIVASI AKUNS',
+            'title' => 'AKTIVASI AKUN',
         ];
         return view('private.aktivasi_akun.view')->with($data);
     }
